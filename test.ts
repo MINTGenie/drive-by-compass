6c4078aff691b136070cf5ce96c2d661ff3d6ba2
MINT Genie <66748747+MINTGenie@users.noreply.github.com>
2021-03-11 03:02:35 +0100

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,8 @@
 // tests go here; this will not be compiled when this package is used as an extension.
+let lr_arr_test: number[] = []
+let pref_head = 100
+basic.forever(function () {
+    l_speed_r_speed_arr = OrientBit.course_correct(pref_head)
+    pins.digitalWritePin(DigitalPin.P0, lr_arr_test[0])
+    pins.digitalWritePin(DigitalPin.P1, lr_arr_test[1])
+})
