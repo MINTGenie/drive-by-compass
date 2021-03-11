6ce8843ae41aa9480785098938b2d7f501075b0b
MINT Genie <66748747+MINTGenie@users.noreply.github.com>
2021-03-11 01:56:19 +0100

    adding extension stuff

---
+++ drive_by_compass.ts
@@ -0,0 +1,66 @@
+// Add your code here
+
+//% color="#4C97FF"
+namespace OrientBit {
+    //% block
+    export function course_correct (preferred_heading: number): number[] {
+        big_diff_speed = 30
+        correction_speed_low = 28
+        correction_speed_high = 35
+        fwd_speed_norm = 32
+        small_err_bounds = 1
+        big_err_bounds = 15
+        while (!(ready)) {
+            basic.pause(2)
+        }
+        get_reading = current_heading
+        if (Math.abs(get_reading - preffered_heading) > big_err_bounds) {
+            if (get_reading <= preferred_heading) {
+                Left_motor_speed = big_diff_speed
+                Right_motor_speed = 0
+            } else if (get_reading >= preferred_heading) {
+                Left_motor_speed = 0
+                Right_motor_speed = big_diff_speed
+            }
+        } else {
+            if (get_reading <= preferred_heading - small_err_bounds) {
+                Left_motor_speed = correction_speed_high
+                Right_motor_speed = correction_speed_low
+            } else if (get_reading >= preferred_heading + small_err_bounds) {
+                Left_motor_speed = correction_speed_low
+                Right_motor_speed = correction_speed_high
+            } else {
+                Left_motor_speed = fwd_speed_norm
+                Right_motor_speed = fwd_speed_norm
+            }
+        }
+        motor_speed_l_r = [Left_motor_speed, Right_motor_speed]
+        return motor_speed_l_r
+    }
+    let sampler = 0
+    let l_speed_r_speed_arr: number[] = []
+    let motor_speed_l_r: number[] = []
+    let Right_motor_speed = 0
+    let Left_motor_speed = 0
+    let current_heading = 0
+    let get_reading = 0
+    let ready = false
+    let big_err_bounds = 0
+    let small_err_bounds = 0
+    let fwd_speed_norm = 0
+    let correction_speed_high = 0
+    let correction_speed_low = 0
+    let big_diff_speed = 0
+
+    control.inBackground(function () {
+        while (true) {
+            for (let index = 0; index <= 4; index++) {
+                sampler += input.compassHeading()
+                basic.pause(2)
+            }
+            current_heading = sampler / 5
+            sampler = 0
+            ready = true
+        }
+    })
+}
--- main.ts
+++ main.ts
@@ -10,61 +10,10 @@ input.onButtonPressed(Button.B, function () {
         preffered_heading = 360 + preffered_heading
     }
 })
-function course_correct (get_read: number, pref: number, diff_motor_speed: boolean) {
-    serial.writeValue("x", get_read)
-    if (diff_motor_speed) {
-        if (get_read <= pref) {
-            Left_motor_speed = 30
-            Right_motor_speed = 0
-        } else if (get_read >= pref) {
-            Left_motor_speed = 0
-            Right_motor_speed = 30
-        }
-    } else {
-        if (get_read <= pref - 1) {
-            Left_motor_speed = 35
-            Right_motor_speed = 28
-        } else if (get_read >= pref + 1) {
-            Left_motor_speed = 28
-            Right_motor_speed = 35
-        } else {
-            Left_motor_speed = 32
-            Right_motor_speed = 32
-        }
-    }
-    motor_speed_l_r = [Left_motor_speed, Right_motor_speed]
-    return motor_speed_l_r
-}
-let l_speed_r_speed_arr: number[] = []
-let get_reading = 0
-let motor_speed_l_r: number[] = []
-let Right_motor_speed = 0
-let Left_motor_speed = 0
-let preffered_heading = 0
-preffered_heading = 255
-let sampler = 0
-let current_heading = 0
-let ready = false
+let preffered_heading = 255
 basic.forever(function () {
-    if (ready) {
-        get_reading = current_heading
-        if (Math.abs(get_reading - preffered_heading) > 15) {
-            l_speed_r_speed_arr = course_correct(get_reading, preffered_heading, true)
-        } else {
-            l_speed_r_speed_arr = course_correct(get_reading, preffered_heading, false)
-        }
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, l_speed_r_speed_arr[0])
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, l_speed_r_speed_arr[1])
-    }
+    l_speed_r_speed_arr = course_correct(preffered_heading)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, l_speed_r_speed_arr[0])
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, l_speed_r_speed_arr[1])
 })
-control.inBackground(function () {
-    while (true) {
-        for (let index = 0; index <= 4; index++) {
-            sampler += input.compassHeading()
-            basic.pause(2)
-        }
-        current_heading = sampler / 5
-        sampler = 0
-        ready = true
-    }
-})
+
