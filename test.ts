4735218332dd3a13b22932b0c70165545710241f
MINT Genie <66748747+MINTGenie@users.noreply.github.com>
2021-03-11 02:22:49 +0100

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,6 @@
 // tests go here; this will not be compiled when this package is used as an extension.
+basic.forever(function () {
+    l_speed_r_speed_arr = OrientBit.course_correct(preffered_heading)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, l_speed_r_speed_arr[0])
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, l_speed_r_speed_arr[1])
+})
