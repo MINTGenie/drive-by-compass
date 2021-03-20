69fdee6bee2557bb9f27574fbcd73a49970e9bf9
MINT Genie <66748747+MINTGenie@users.noreply.github.com>
2021-03-20 16:24:38 +0100

    remove maqueen related info

--- main.ts
+++ main.ts
@@ -1,18 +1,1 @@
-input.onButtonPressed(Button.A, function () {
-    preffered_heading += 15
-    if (preffered_heading > 360) {
-        preffered_heading = preffered_heading - 360
-    }
-})
-input.onButtonPressed(Button.B, function () {
-    preffered_heading += -15
-    if (preffered_heading < 0) {
-        preffered_heading = 360 + preffered_heading
-    }
-})
-let l_speed_r_speed_arr: number[] = []
-let preffered_heading = 0
-preffered_heading = 120
-basic.forever(function () {
-    l_speed_r_speed_arr = OrientBit.course_correct(preffered_heading, 30)
-})
+
